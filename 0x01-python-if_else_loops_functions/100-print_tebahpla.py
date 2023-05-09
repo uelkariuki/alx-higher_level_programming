1b41d37e05ff1ff20d3ed526ff780504b4b9127f
uelkariuki <cratzuel@gmail.com>
2023-05-09 22:41:44 +0000

    task 14 advanced

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 for a in range(122, 96, -1):
-    print(chr(a) if a % 2 == 0 else chr(a - 32), end="")
+    print('{:s}'.format(chr(a)) if a % 2 == 0 else '{:s}'.format(chr(a - 32)), end="")
