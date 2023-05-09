9d872850fe2942f9139a8e864b9ddf7846087648
uelkariuki <cratzuel@gmail.com>
2023-05-09 19:36:17 +0000

    task 7

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 
 def islower(c):
+    if not c:
+        raise ValueError
     if len(c) != 1:
         return False
-
-    the_ascii = ord(c)
-
-    if (97 <= the_ascii <= 122):
-        return True
-    return False
+    else:
+        return (ord(c) >= 97 and ord(c) <= 122)
