84dfb85cfd183b4b38725a75bc7aeaf9b6409d5a
uelkariuki <cratzuel@gmail.com>
2023-05-11 22:59:19 +0000

    task 8 Advanced

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -6,7 +6,7 @@ def magic_calculation(a, b):
     if a < b:
         c = add(a, b)
         for i in range(4, 7):
-            c = add(c + i)
+            c = add(c, i)
             return c
-        else:
-            return sub(a, b)
+    else:
+        return sub(a, b)
