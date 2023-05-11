f356f04247f9680129284aee86c8cf130110955e
uelkariuki <cratzuel@gmail.com>
2023-05-11 11:36:25 +0000

    task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -5,6 +5,6 @@ from add_0 import add
 if __name__ == "__main__":
     a = 1
     b = 2
-    print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)), end="")
+    print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
 else:
     ValueError
