393dfaa4a941771080eea62444ebebdfd23374d7
uelkariuki <cratzuel@gmail.com>
2023-05-11 13:26:35 +0000

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,11 +4,9 @@ import sys
 
 if __name__ == "__main__":
     a = len(sys.argv[1:])
-    if a == 0:
-        print('{}'. format(a), "arguments.", end=" ")
-    elif a == 1:
+    if a == 1:
         print('{}'. format(a), "argument:")
-    elif a >= 2:
+    else:
         print('{}'. format(a), "arguments.")
 
     arg_position = 1
