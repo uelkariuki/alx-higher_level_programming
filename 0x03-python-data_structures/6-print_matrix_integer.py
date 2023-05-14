4606e00f2c387b1ec799dd893de75589b2fdd966
uelkariuki <cratzuel@gmail.com>
2023-05-14 18:22:58 +0000

    task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for q in range(len(row)):
             number = row[q]
-            if q == len(row) -1:
+            if q == len(row) - 1:
                 print("{:d}".format(number), end="")
             else:
                 print("{:d}".format(number), end=" ")
