e8df1a4a1b934e65b1c0494c9913bf838d6d7012
uelkariuki <cratzuel@gmail.com>
2023-05-17 16:17:10 +0000

    task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,5 +2,4 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
-    sorted_dict = sorted(a_dictionary)
-    return sorted_dict[-1]
+    return max(a_dictionary, key=a_dictionary.get)
