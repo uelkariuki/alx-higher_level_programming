4272f32d6ad5e1668a708697eadf0517f94220e2
uelkariuki <cratzuel@gmail.com>
2023-05-17 19:53:13 +0000

    task 13

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    score = [item[0] for item in my_list]
+    weight = [item[1] for item in my_list]
+    weighted_sum = sum(score * weight for score, weight in zip(score, weight))
+    total_sum_weight = sum(weight)
+    weight_average = weighted_sum / total_sum_weight
+    return weight_average
