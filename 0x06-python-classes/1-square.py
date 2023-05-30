21bb411832a6117c9d4bded69d38e97145143a40
uelkariuki <cratzuel@gmail.com>
2023-05-30 14:57:51 +0000

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""
+A class Square that defines a square by: (based on 0-square.py
+"""
+
+
+class Square:
+    """ class square"""
+    def __init__(self, size=None):
+        self.__size = size
