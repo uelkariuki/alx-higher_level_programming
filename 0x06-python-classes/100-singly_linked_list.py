b3a64c7257d09bdfcd43b3d22022984e90463aef
uelkariuki <cratzuel@gmail.com>
2023-05-31 20:04:07 +0000

    task 7.1

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -63,7 +63,11 @@ class SinglyLinkedList:
         """
         new_node = Node(value)
 
-        if self.head is None or self.head.data > value:
+        if self.head is None:
+            self.head = new_node
+        elif isinstance(value, str):
+            raise TypeError("data must be an integer")
+        elif self.head.data > value:
             new_node.next_node = self.head
             self.head = new_node
         else:
