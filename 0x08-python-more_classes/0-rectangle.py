e5705877ac443a65f96d1c2d60d9f91ae686a7d5
uelkariuki <cratzuel@gmail.com>
2023-06-05 10:34:42 +0000

    task 0

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+A module that handles writing an empty class Rectangle\
+that defines a rectangle
+"""
+
+
+class Rectangle:
+    """ empty class Rectangle"""
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,5 @@
+# 0x08. Python - More Classes and Objects
+## Tasks
+
+0. Write an empty class `Rectangle` that defines a rectangle
+
