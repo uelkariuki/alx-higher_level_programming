56a77048799c2012fbb4c41dbce8052eff3b0b8b
uelkariuki <cratzuel@gmail.com>
2023-06-05 19:46:56 +0000

    task 8

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -122,9 +122,9 @@ class Rectangle:
                 returns the biggest rectangle based on the area
         """
         if not isinstance(rect_1, Rectangle):
-            raise ValueError("rect_1 must be an instance of Rectangle")
+            raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
-            raise ValueError("rect_1 must be an instance of Rectangle")
+            raise TypeError("rect_1 must be an instance of Rectangle")
 
         if rect_1.area() == rect_2.area():
             return rect_1
