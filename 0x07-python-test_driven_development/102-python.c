9030f927804426e4aace7fb385ee25f84a2ebba3
uelkariuki <cratzuel@gmail.com>
2023-06-06 22:21:09 +0000

    task 8unf

---
+++ 0x07-python-test_driven_development/102-python.c
@@ -0,0 +1,30 @@
+#include <Python.h>
+
+/**
+ * print_python_string - function that prints Python strings.
+ * @p: pointer to a Python Bytes Object
+ */
+
+void print_python_string(PyObject *p)
+{
+	const char *value, *the_name_type;
+	PySsize_t length;
+	PyObject *type;
+
+	if (PyUnicode_Check(p))
+	{
+		length = PyUnicode_GET_LENGTH(p);
+		value = PyUnicode_ASUTF8(p);
+		type = (PyObject *)Py_TYPE(p);
+		the_name_type = Py_TYPE(type)->tp_name;
+
+		printf("[.] string object info\n");
+		printf("  type: %s\n", the_name_type);
+		printf("  length: %d\n", length);
+		printf("  value: %s\n", value);
+	}
+	else
+	{
+		printf("  [ERROR] Invalid String Object\n")
+	}
+}
