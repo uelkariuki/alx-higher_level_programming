8a8005d6d07ab4afaf821c476abd6333a29bb645
uelkariuki <cratzuel@gmail.com>
2023-06-06 19:48:50 +0000

    Task 30. Low memory cost

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+Write a class LockedClass with no class or
+object attribute, that prevents the user from dynamically
+creating new instance attributes, except if the new instance
+attribute is called first_name.
+"""
+
+
+class LockedClass():
+    """ class LOckedClass """
+
+    def __setattr__(self, name, value):
+        if name != "first_name":
+            raise AttributeError(f"'{self.__class__.__name__}'\
+ object has no attribute '{name}'")
+        super().__setattr__(name, value)
