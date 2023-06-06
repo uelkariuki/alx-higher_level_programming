35cc0aafe3c1db22ebe91d32371714036a343881
uelkariuki <cratzuel@gmail.com>
2023-06-06 22:41:53 +0000

    task 8

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -11,20 +11,19 @@ void print_python_string(PyObject *p)
 	Py_ssize_t length;
 	PyObject *type;
 
-	if (PyUnicode_Check(p))
-	{
-		length = PyUnicode_GET_LENGTH(p);
-		value = PyUnicode_AsUTF8(p);
-		type = (PyObject *)Py_TYPE(p);
-		the_name_type = Py_TYPE(type)->tp_name;
-
-		printf("[.] string object info\n");
-		printf("  type: %s\n", the_name_type);
-		printf("  length: %zd\n", length);
-		printf("  value: %s\n", value);
-	}
-	else
+	if (p = NULL || !PyUnicode_Check(p))
 	{
 		printf("  [ERROR] Invalid String Object\n");
+		return;
 	}
+
+	length = PyUnicode_GET_LENGTH(p);
+	value = PyUnicode_AsUTF8(p);
+	type = (PyObject *)Py_TYPE(p);
+	the_name_type = Py_TYPE(type)->tp_name;
+
+	printf("[.] string object info\n");
+	printf("  type: %s\n", the_name_type);
+	printf("  length: %zd\n", length);
+	printf("  value: %s\n", value);
 }
