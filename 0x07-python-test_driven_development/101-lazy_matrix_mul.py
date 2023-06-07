01fa53888c523a9bfb6104d2d15d7752d14f788e
uelkariuki <cratzuel@gmail.com>
2023-06-07 16:49:23 +0000

    Task 7.1 correction

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -21,30 +21,38 @@ def lazy_matrix_mul(m_a, m_b):
         raise TypeError("m_b must be a list")
     if any(not isinstance(row, list) for row in m_b):
         raise TypeError("m_b must be a list of lists")
-    if (m_a == []) or (m_a == [[]]):
-        raise ValueError("m_a can't be empty")
-    if (m_b == []) or (m_b == [[]]):
-        raise ValueError("m_b can't be empty")
+    if (m_a == []):
+        raise ValueError("shapes (0,) and (2,2) not aligned: 0\
+ (dim 0) != 2 (dim 0)")
+    if (m_a == [[]]):
+        raise ValueError("shapes (1,0) and (2,2) not aligned: 0\
+ (dim 1) != 2 (dim 0)")
+    if (m_b == []):
+        raise ValueError("shapes (2,2) and (0,) not aligned: 2\
+ (dim 1) != 0 (dim 0)")
+    if (m_b == [[]]):
+        raise ValueError("shapes (2,2) and (1,0) not aligned: 2\
+ (dim 1) != 1 (dim 0)")
     for row in m_a:
         for element in row:
             if not isinstance(element, (int, float)):
-                raise TypeError("m_a should contain only integers or floats")
+                raise TypeError("invalid data type for einsum")
 
     for row in m_b:
         for element in row:
             if not isinstance(element, (int, float)):
-                raise TypeError("m_b should contain only integers or floats")
+                raise TypeError("invalid data type for einsum")
 
     no_column_a = len(m_a[0])
     if any(len(row) != no_column_a for row in m_a):
-        raise TypeError("each row of m_a must be of the same size")
+        raise ValueError("setting an array element with a sequence.")
 
     no_column_b = len(m_b[0])
     if any(len(row) != no_column_b for row in m_b):
-        raise TypeError("each row of m_b must be of the same size")
+        raise ValueError("setting an array element with a sequence.")
 
-    if no_column_a != len(m_b):
-        raise ValueError("m_a and m_b can't be multiplied")
+    # if no_column_a != len(m_b):
+        # raise ValueError("m_a and m_b can't be multiplied")
 
     result = np.dot(m_a, m_b)
     return result
