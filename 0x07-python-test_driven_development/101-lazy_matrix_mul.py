ae018bd206f3b4a3ca8cb8597669333571730781
uelkariuki <cratzuel@gmail.com>
2023-06-07 02:02:14 +0000

    Task 7. Lazy matrix multiplication

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 """
-Write a function that multiplies 2 matrices by using the module NumPy
+A module to handle writing a function that multiplies
+2 matrices by using the module NumPy
 """
 
 import numpy as np
@@ -9,7 +10,41 @@ import numpy as np
 importing numpy
 """
 
+
 def lazy_matrix_mul(m_a, m_b):
-    """ a function to multiply 2 matrices using NumPy"""
+    """ A function to multiply 2 matrices using NumPy"""
+    if not isinstance(m_a, list):
+        raise TypeError("m_a must be a list")
+    if any(not isinstance(row, list) for row in m_a):
+        raise TypeError("m_a must be a list of lists")
+    if not isinstance(m_b, list):
+        raise TypeError("m_b must be a list")
+    if any(not isinstance(row, list) for row in m_b):
+        raise TypeError("m_b must be a list of lists")
+    if (m_a == []) or (m_a == [[]]):
+        raise ValueError("m_a can't be empty")
+    if (m_b == []) or (m_b == [[]]):
+        raise ValueError("m_b can't be empty")
+    for row in m_a:
+        for element in row:
+            if not isinstance(element, (int, float)):
+                raise TypeError("m_a should contain only integers or floats")
+
+    for row in m_b:
+        for element in row:
+            if not isinstance(element, (int, float)):
+                raise TypeError("m_b should contain only integers or floats")
+
+    no_column_a = len(m_a[0])
+    if any(len(row) != no_column_a for row in m_a):
+        raise TypeError("each row of m_a must be of the same size")
+
+    no_column_b = len(m_b[0])
+    if any(len(row) != no_column_b for row in m_b):
+        raise TypeError("each row of m_b must be of the same size")
+
+    if no_column_a != len(m_b):
+        raise ValueError("m_a and m_b can't be multiplied")
+
     result = np.dot(m_a, m_b)
     return result
