a7e734c73b0d5e8cb138f45f802f8647301e2c31
uelkariuki <cratzuel@gmail.com>
2023-06-07 14:40:51 +0000

    Task 4

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -24,6 +24,6 @@ def text_indentation(text):
     """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    a = text.replace(".", ".\n\n").replace("?", "?\n\n").replace(":", ":\n\n")
+    a = text.replace("?", "?\n\n").replace(".", ".\n\n").replace(":", ":\n\n")
 
     print(a, end="")
