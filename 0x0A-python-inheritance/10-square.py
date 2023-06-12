851f899e70e245c789cf7d8117ff6ba37d31cebb
uelkariuki <cratzuel@gmail.com>
2023-06-12 19:39:30 +0000

    Task 9

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+Write a class Square that inherits from Rectangle (9-rectangle.py):
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+""" class Rectangle that the square class will inherit from"""
+
+
+class Square(Rectangle):
+    """ class square that inherits from Rectangle"""
+    def __init__(self, size):
+        """ Instantiation with size"""
+        super().__init__(size, size)
+        self.__size = size
