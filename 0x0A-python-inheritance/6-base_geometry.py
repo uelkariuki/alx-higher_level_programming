29ebe46515aa867b8bdcaacff13d3863b4247fea
uelkariuki <cratzuel@gmail.com>
2023-06-12 14:41:15 +0000

    Task 6.  Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+Write a class BaseGeometry (based on 5-base_geometry.py).
+Public instance method: def area(self): that raises
+an Exception with the message area() is not implemented
+"""
+
+
+class BaseGeometry:
+    """
+    class  BaseGeometry
+    """
+
+    def area(self):
+        """
+        Public instance method that raises an
+        Exception with the message area() is not implemented
+        """
+        raise Exception("area() is not implemented")
