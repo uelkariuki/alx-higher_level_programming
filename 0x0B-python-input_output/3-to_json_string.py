c88c9ed5381cb53595e02501d3f5974682f1917f
uelkariuki <cratzuel@gmail.com>
2023-06-13 16:09:45 +0000

    Task 3. To JSON string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+Write a function that returns the JSON representation of an object (string)
+"""
+
+import json
+""" importing the json module"""
+
+
+def to_json_string(my_obj):
+    """
+    Function that returns the JSON representation of an object (string)
+    """
+
+    json_object = json.dumps(my_obj)
+    return json_object
