995ad792c080d5b2645316d9fc2529aca3106d23
uelkariuki <cratzuel@gmail.com>
2023-06-13 15:12:11 +0000

    Task 0.2

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -9,11 +9,6 @@ def read_file(filename=""):
     """
     Function that reads a text file (UTF8) and prints it to stdout
     """
-    try:
-        with open(filename, encoding="utf-8") as f:
-            read_text = f.read()
-            print(read_text)
-    except PermissionError:
-        print("Permission denied")
-    except Exception as e:
-        print("Error: {}".format(e))
+    with open(filename, encoding="utf-8") as f:
+        for line in f:
+            print(line, end="")
