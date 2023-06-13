a66ed217369fc9eb1d497478eabedadc62313db6
uelkariuki <cratzuel@gmail.com>
2023-06-13 19:15:59 +0000

    Task 8. Class to JSON

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+Write a function that returns the dictionary description with
+simple data structure (list, dictionary, string, integer and
+boolean) for JSON serialization of an object:
+"""
+
+
+def class_to_json(obj):
+    """
+    Function that returns the dictionary description with
+    simple data structure for JSON serialization of an object
+    """
+
+    return obj.__dict__
