14837e5024d114027aca0f10d7fa5553530a5147
uelkariuki <cratzuel@gmail.com>
2023-06-14 20:41:17 +0000

    Task 14.2 Log parsing

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -15,20 +15,21 @@ total_size = 0
 
 try:
     for line in sys.stdin:
-        line_counter = line_counter + 1
-        data = line.split()
-        status_code = int(data[-2])
-        file_size = int(data[-1])
-        total_size += file_size
-        if status_code in code_count:
-            code_count[status_code] += 1
-        if line_counter % 10 == 0:
-            print("File size: {}".format(total_size))
-            for code in sorted(code_count.keys()):
-                if code_count[code] > 0:
-                    print("{}: {}".format(code, code_count[code]))
-except (ValueError, IndexError):
-    pass
+        try:
+            line_counter = line_counter + 1
+            data = line.split()
+            status_code = int(data[-2])
+            file_size = int(data[-1])
+            total_size += file_size
+            if status_code in code_count:
+                code_count[status_code] += 1
+            if line_counter % 10 == 0:
+                print("File size: {}".format(total_size))
+                for code in sorted(code_count.keys()):
+                    if code_count[code] > 0:
+                        print("{}: {}".format(code, code_count[code]))
+        except (ValueError, IndexError):
+            pass
 except KeyboardInterrupt:
     pass
 finally:
