8133acede5c40f1a7fac1f30a2d89bc302341be7
uelkariuki <cratzuel@gmail.com>
2023-06-14 20:16:09 +0000

    Task 14. Log parsing

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -5,34 +5,32 @@ Write a script that reads stdin line by line and computes metrics
 """
 import sys
 """ importing the sys module to use std input"""
-import re
-""" importing regex module"""
 
 
-status_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+status_codes = [200, 301, 400, 401, 403, 404, 405, 500]
 
-stats = {'file size': 0, 'status_codes': {code: 0 for code in status_codes}}
+code_count = {code: 0 for code in status_codes}
+line_counter = 0
+total_size = 0
 
 try:
-    line_counter = 0
     for line in sys.stdin:
         line_counter = line_counter + 1
-        match = re.search(r'"GET /projects/260 HTTP/1.1" (\d{3} (\d+))', line)
-        if match:
-            status_code = match.group(1)
-            file_size = match.group(2)
-            stats['file size'] += int(file_size)
-            if status_code in status_codes:
-                stats['status_codes'][status_code] += 1
+        data = line.split()
+        status_code = int(data[-2])
+        file_size = int(data[-1])
+        total_size += file_size
+        if status_code in code_count:
+            code_count[status_code] += 1
         if line_counter % 10 == 0:
-            print("File size: {}".format(stats['file size']))
-            for code in sorted(stats['status_codes']):
-                if stats['status_codes'][code] > 0:
-                    print("{}: {}".format(code, (stats['status_codes'][code])))
-            print()
-
+            print("File size: {}".format(total_size))
+            for code in sorted(code_count.keys()):
+                if code_count[code] > 0:
+                    print("{}: {}".format(code, code_count[code]))
 except KeyboardInterrupt:
-    print("File size: {}".format(stats['file size']))
-    for code in sorted(stats['status_codes']):
-        if stats['status_codes'][code] > 0:
-            print("{}: {}".format(code, (stats['status_codes'][code])))
+    pass
+finally:
+    print("File size: {}".format(total_size))
+    for code in sorted(code_count.keys()):
+        if code_count[code] > 0:
+            print("{}: {}".format(code, code_count[code]))
