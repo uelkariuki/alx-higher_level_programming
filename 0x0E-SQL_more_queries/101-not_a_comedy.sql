6317537f8b50001c51146836180892989ca13e37
uelkariuki <cratzuel@gmail.com>
2023-07-20 01:42:03 +0000

    Task 18. No Comedy tonight!

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -10,8 +10,10 @@
 
 SELECT tv_shows.title
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
-LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.show_id
-WHERE tv_genres.name != "Comedy"
-GROUP BY tv_shows.title
+WHERE tv_shows.id NOT IN (
+	SELECT tv_show_genres.show_id
+	FROM tv_show_genres
+	JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+	WHERE tv_genres.name = "Comedy"
+)
 ORDER BY tv_shows.title ASC;
