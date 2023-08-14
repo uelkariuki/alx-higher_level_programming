8ba4b3e9b7d3eca5faa6378a54c68b6dd55ef72d
uelkariuki <cratzuel@gmail.com>
2023-08-14 16:12:43 +0000

    Task 2. Arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const argv = process.argv;
+
+if (argv.length === 3) {
+  console.log('Argument found');
+} else if (argv.length > 3) {
+  console.log('Arguments found');
+} else {
+  console.log('No argument');
+}
