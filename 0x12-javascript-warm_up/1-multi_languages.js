d32509a95c964d6520b018a586a8ec172bed1dca
uelkariuki <cratzuel@gmail.com>
2023-08-14 15:11:09 +0000

    Task 1. 3 languages

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const C = 'C is fun';
+const Python = 'Python is cool';
+const Javascript = 'JavaScript is amazing';
+
+console.log(C);
+console.log(Python);
+console.log(Javascript);
