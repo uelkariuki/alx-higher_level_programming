8d1b5794dfdfcdf79ee47ee4b717036b3b115ad0
uelkariuki <cratzuel@gmail.com>
2023-08-15 23:07:13 +0000

    Fix inheritance from 5-square.js issue

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,35 +1,6 @@
 #!/usr/bin/node
 
-class Rectangle {
-  constructor (w, h) {
-    if (w <= 0 || !Number.isInteger(w) || h <= 0 || !Number.isInteger(h)) {
-      return 'Rectangle {}';
-    } else {
-      this.width = w;
-      this.height = h;
-    }
-  }
-
-  print () {
-    for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
-    }
-  }
-
-  rotate () {
-    [this.width, this.height] = [this.height, this.width];
-  }
-
-  double () {
-    [this.width, this.height] = [this.width * 2, this.height * 2];
-  }
-}
-
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
+module.exports = class Square extends require('./5-square.js') {
   charPrint (c) {
     if (c === undefined) {
       c = 'X';
@@ -38,6 +9,4 @@ class Square extends Rectangle {
       console.log(c.repeat(this.width));
     }
   }
-}
-
-module.exports = Square;
+};
