ec70740880a3f272d0335a9455cf3e2f619b7cb9
uelkariuki <cratzuel@gmail.com>
2023-08-19 00:56:11 +0000

    Task 2. Filter states by user input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 """
-script that lists all states with a name starting with N (upper N) from
-the database hbtn_0e_0_usa
+script that takes in an argument and displays all values in the
+states table of hbtn_0e_0_usa where name matches the argument.
 """
 
 import MySQLdb
@@ -14,6 +14,7 @@ if __name__ == "__main__":
     mysql_username = sys.argv[1]
     mysql_password = sys.argv[2]
     database_name = sys.argv[3]
+    search_name = sys.argv[4]
     MySQL_host = "localhost"
     MySQL_port = 3306
 
@@ -23,11 +24,13 @@ if __name__ == "__main__":
             user=mysql_username,
             passwd=mysql_password,
             db=database_name
+
             )
 
     cursor = db_conn.cursor()
-    cursor.execute("SELECT * FROM states WHERE name\
-        LIKE 'N%' ORDER BY states.id ASC")
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY\
+            states.id ASC".format(search_name)
+    cursor.execute(query)
 
     states_list = cursor.fetchall()
     for row in states_list:
