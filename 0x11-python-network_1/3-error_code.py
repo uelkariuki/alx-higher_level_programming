7223fec570f71e5d40befcb4c2527a53c2285da0
uelkariuki <cratzuel@gmail.com>
2023-09-01 10:45:07 -0700

    Task 3. Error code #0

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+Python script that takes in a URL, sends a request to the URL and
+displays the body of the response (decoded in utf-8)
+"""
+
+import sys
+import urllib.request
+""" Importing the required modules"""
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    try:
+        with urllib.request.urlopen(url) as response:
+            html = response.read()
+            print(html.decode("utf-8"))
+    except urllib.error.HTTPError as e:
+        print("Error code:", e.code)
