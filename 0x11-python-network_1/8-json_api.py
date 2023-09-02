6f537fc27f99aaeb5b79b4fe24c7667801d1432e
uelkariuki <cratzuel@gmail.com>
2023-09-01 18:34:17 -0700

    Task 8. Search API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     if len(sys.argv) > 1:
         q = sys.argv[1]
     else:
-        q=""
+        q = ""
     q_data = {'q': q}
     response = requests.post('http://0.0.0.0:5000/search_user', data=q_data)
 
@@ -27,6 +27,3 @@ if __name__ == "__main__":
             print('No result')
     except json.JSONDecodeError:
         print('Not a valid JSON')
-
-
-
