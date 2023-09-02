6b2ceca5a4f6ff5b66e08ce2e8b1d74257b18313
uelkariuki <cratzuel@gmail.com>
2023-09-01 19:55:49 -0700

    Task 10. Time for an interview!

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+"""
+Please list 10 commits (from the most recent to
+oldest) of the repository “rails” by the user “rails”
+You must use the GitHub API, here is the
+documentation https://developer.github.com/v3/repos/commits/
+Print all commits by: `<sha>: <author name>` (one by line)
+Python script that takes 2 arguments in order to solve this challenge.
+"""
+
+import sys
+import requests
+""" Importing the required modules"""
+
+
+def github_commits(repository, owner):
+    """Method to help get the 10 commits from Github"""
+
+    url = f"https://api.github.com/repos/{owner}/{repository}/commits"
+
+    response = requests.get(url)
+    github_commits = response.json()
+
+    for commit in github_commits[:10]:
+        sha = commit['sha']
+        name_author = commit["commit"]["author"]["name"]
+        print(f'{sha}:{name_author}')
+
+
+if __name__ == "__main__":
+    repository = sys.argv[1]
+    owner = sys.argv[2]
+    github_commits(repository, owner)
