4ca2ff9d1b3618bf0342c6aefee618c65c7ec5cb
uelkariuki <cratzuel@gmail.com>
2023-09-01 17:37:32 -0700

    Task 7. Error code #1

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""
+Python script that takes in a URL, sends a request to the URL and
+displays the body of the response.
+"""
+
+import requests
+import sys
+""" Importing the sys and requests modules"""
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        print(response.text)
+
+    except requests.exceptions.HTTPError as e:
+        print("Error code:",e.response.status_code)
+
+
